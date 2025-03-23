620293374c9706e0306b8ee610ab6c3f84c20767
Ananya <afmtrendaid@gmail.com>
2025-03-23 18:23:55 +0300

    Bet, Game and Transaction proto schema and generated package

--- api/generated/proto/userpb/user_grpc.pb.go
+++ api/generated/proto/userpb/user_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v5.29.3
-// source: proto/user.proto
+// source: api/proto/user.proto
 
 package userpb
 
@@ -155,5 +155,5 @@ var UserService_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "proto/user.proto",
+	Metadata: "api/proto/user.proto",
 }
